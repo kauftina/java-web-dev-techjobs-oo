9ed8386cb88a0ddec7e57f0b770b7d181f1dcf77
kauftina <67398663+kauftina@users.noreply.github.com>
2020-09-20 20:42:30 -0500

    git commit -m "finished 2 and 3" git commit -m "Test 1"

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,17 +1,21 @@
-package Tests;
+package org.launchcode.techjobs_oo.Tests;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
+import org.launchcode.techjobs_oo.*;
 
 public class JobTest {
     Job job1;
     Job job2;
+    Job job3;
 
+    
     @Before
     public void createCarObject() {
         job1 = new Job();
         job2 = new Job();
+        job3 = new Job();
     }
 
     @Test
@@ -20,4 +24,10 @@ public class JobTest {
         assertEquals(1, job2.getId()-job1.getId());
 
     }
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+    }
+
 }
